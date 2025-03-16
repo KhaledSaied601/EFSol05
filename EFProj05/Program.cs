8c31d2c8dc14e54f9f1b315102c2ce854fd456d3
KhaledSaied601 <ksaed110@gmail.com>
2025-03-16 11:45:08 +0200

    Add project files.

---
+++ EFProj05/Program.cs
@@ -0,0 +1,22 @@
+﻿using EFProj05.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFProj05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (MyNorthwindContext myNorthwindContext = new MyNorthwindContext(new Microsoft.EntityFrameworkCore.DbContextOptions<MyNorthwindContext>()))
+            {
+
+
+                myNorthwindContext.Categories.FromSqlInterpolated($"update Categories set CategoryName='Khaled' where CategoryID=8");
+
+
+
+
+            }
+        }
+    }
+}
